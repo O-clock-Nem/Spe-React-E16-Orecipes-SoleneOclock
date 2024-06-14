ab0e11280c8adbd4f9225a9568362b929cc05545
SoleneOclock <solene@oclock.io>
2024-06-14 12:01:53 +0200

    au chargmenet de la page Favorites on demande au thunk getFavRecepe de fetch les fav recipes puis lui meme demande au reducer de les enregistrer dans le state et dans la page Favorite on les lit depuisle state pour les afficher : attention faudrait supprimer les recette quand on se deconnexte sinon au firstrender elles sont encore là avant le fetch et le second render ou y'ava les nouvelles

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,12 +1,24 @@
+import { useEffect } from 'react';
+import getFavRecipes from '../../store/thunks/getFavRecipes';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import Page from '../Page';
 import AppHeader from '../AppHeader';
 import Content from '../Content';
 
 function Favorites() {
+  const dispatch = useAppDispatch();
+
   // le user arrive sur cette page par le navlink "mes recettes pref" donc il est connecté
   // mais si il est pas connecté et qu'il tape le lien dans l'URL direct il faut ne pas afficher cette page
-  // on recupere dans le state les recettes pref du user connecté
-  const recipesPref = [];
+  // au premier chargement de la page favorites on fait une demande de fetch des recettes pref et de sauvegarde dans le state
+  useEffect(() => {
+    // - fetch les recettes pref -> thunk
+    // - les enregistrer dans le state -> reducer
+    dispatch(getFavRecipes());
+  }, []);
+
+  // on lit dans le state les recettes pref du user connecté
+  const recipesPref = useAppSelector((state) => state.recipes.listFav);
 
   return (
     <Page>
--- src/store/reducers/recipes.ts
+++ src/store/reducers/recipes.ts
@@ -2,14 +2,17 @@ import { createReducer } from '@reduxjs/toolkit';
 // import data from '../../data'; // données bidons
 import { Recipe } from '../../@types/recipe';
 import getRecipes from '../thunks/getRecipes';
+import getFavRecipes from '../thunks/getFavRecipes';
 
 interface RecipesState {
   list: Recipe[];
+  listFav: Recipe[];
   loading: boolean;
   errorMessage: null | string;
 }
 export const initialState: RecipesState = {
   list: [], // au debut pas de recettes, on va remplir le tableau après le fetch
+  listFav: [], // au debut pas de recettes pref, on va remplir le tableau après le fetch
   loading: true, // au debut pas de recettes donc on veut le loader donc loading à true
   errorMessage: null,
 };
@@ -26,6 +29,9 @@ const recipesReducer = createReducer(initialState, (builder) => {
       state.errorMessage = `Erreur lors de la récupération des recettes : ${action.error}`;
       // on passe le loader à false pour ne pas que le user croie qu'on est encore en train de loader les datas alors que y'a eu une error, il les recevra jamais
       state.loading = false;
+    })
+    .addCase(getFavRecipes.fulfilled, (state, action) => {
+      state.listFav = action.payload;
     });
 });
 
