48d8a08b5deb0d327afac289e327d6f302a76799
SoleneOclock <solene@oclock.io>
2024-06-14 09:32:57 +0200

    controle en lecture des inputs ok

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,18 +1,21 @@
 import './styles.scss';
 import logo from '../../assets/logo.png';
 import LoginForm from '../LoginForm';
+import { useAppSelector } from '../../hooks/redux';
 
 function AppHeader() {
   // X on va ajouter email et password dans le state redux pour input controlés
-  // on recupère la valeur de email et password pour filer à LoginForm
+  // X on recupère la valeur de email et password pour filer à LoginForm
+  const email = useAppSelector((state) => state.user.credentials.email);
+  const password = useAppSelector((state) => state.user.credentials.password);
   // dans changeField (au change des inputs) on va aller modifier la valeur de email ou password dans le state redux (dispatch action)
 
   return (
     <header className="header">
       <img src={logo} className="header-logo" alt="Logo oRecipes" />
       <LoginForm
-        email="email"
-        password="ps"
+        email={email}
+        password={password}
         changeField={(value: string, name: 'email' | 'password') => {
           console.log('changeFiel executé');
           console.log('value', value);
